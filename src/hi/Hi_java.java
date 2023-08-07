a15505121a6fe58f1b3a03199b833eabdcb1f559
eomhyunjong <djaguswhd0306@naver.com>
2023-08-07 12:43:41 +0900

    key

--- src/hi/Hi_java.java
+++ src/hi/Hi_java.java
@@ -6,6 +6,7 @@ public class Hi_java {
 		System.out.println("Hello");
 		System.out.println("성공했네~ 기쁘네");
 		System.out.println("token유지바람");
+		System.out.println("key_업로드 안 되게 폴더 밖으로 뺀다.");
 	}
 
 }
